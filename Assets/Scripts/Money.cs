4ab0817ceb869944f1cef658de8bad2fc233455b
ramjsandal <61168190+ramjsandal@users.noreply.github.com>
2025-02-08 01:02:45 -0500

    moeny now a trigger

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Money : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision != null && collision.gameObject.CompareTag("Player"))
         {
