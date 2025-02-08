19a0e72936070cbb962f08dc3e51bc1b9a6b8f82
ramjsandal <61168190+ramjsandal@users.noreply.github.com>
2025-02-08 14:46:29 -0500

    toll booth working with text and sprite

--- Assets/Scripts/TollGate.cs
+++ Assets/Scripts/TollGate.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TollGate : MonoBehaviour
 {
     [SerializeField]
     int price;
+
+    [SerializeField]
+    TMP_Text priceText;
+
+    private void Start()
+    {
+        priceText.text = price.ToString();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.transform.CompareTag("Player") && MoneyPool.Instance.playerMoney >= price)
