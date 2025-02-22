6226f5d2ca64cb6fdef9b05943e39eaa27014034
ramjsandal <61168190+ramjsandal@users.noreply.github.com>
2025-02-22 17:18:23 -0500

    barrel sorted

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Barrel : MonoBehaviour
 {
+    static Player player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (player == null)
+        {
+            player = GameObject.FindObjectOfType<Player>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // If we collide with the player, check the player's speed
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision != null && collision.gameObject.CompareTag("Player"))
+        {
+
+            Vector2 vel = player.GetVelocity();
+            // if player going fast enough destroy barrel
+            if (vel.magnitude > .0075f)
+            {
+
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
